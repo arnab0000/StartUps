8c64d85348cfbbff4175a8f0581e118e6ff77695
Arnab Banerjee <arnab@Arnabs-MacBook-Pro.local>
2020-08-06 09:44:46 +0530

    solution1

---
+++ CaseStudy/solution2.py
@@ -0,0 +1,5 @@
+# 2. Even after trying for so many times, your friend’s startup could not find the investment. So you decided to take this matter
+#    in your hand and try to find the list of investors who probably can invest in your friend’s startup. Your list will increase
+#    the chance of your friend startup getting some initial investment by contacting these investors. Find the top 5 investors who
+#    have invested maximum number of times (consider repeat investments in one company also). In a startup, multiple investors might
+#    have invested. So consider each investor for that startup. Ignore undisclosed investors.
